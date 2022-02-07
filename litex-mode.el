d8415c01ffc000e5c2b99910eeefd626439b03b2
Gaurav <allmanpride@gmail.com>
2022-02-07 13:26:57 -0500

    Run doccheck; add doctrings & variables

--- litex-mode.el
+++ litex-mode.el
@@ -24,33 +24,58 @@
 
 ;;; Commentary:
 
-;; converts valid mathematical lisp expressions to LaTeX snippets
+;; converts valid mathematical Lisp expressions to LaTeX snippets
 ;; improved from https://emacs.stackexchange.com/a/70360
 ;; modified with help from https://gist.github.com/bpanthi977/4b8ece0eeff3bc05bb82275a23cbb56d
 
 ;;; Code:
-(eval-when-compile (require 'pcase)) 
+(eval-when-compile (require 'pcase))
 (require 'cl-lib)
 
+
+;; lisp functions that lave their own latex commands
 (defvar litex-latex-functions '(sin cos tan))
+
+;; enclose latex converted to paran
 (defvar litex-latex-maybe-enclose? nil)
 
+;; format string to be used by floats, and their limits
+(defvar litex-format-float-string "%.3f")
+(defvar litex-format-float-upper-limit 1e4)
+(defvar litex-format-float-lower-limit 1e-2)
 
-(defvar litex-format-string "%.2f")
+;; used for joing strings in steps of solution
 (defvar litex-steps-join-string "= ")
 (defvar litex-steps-end-string " ")
 
+;; math inlin environment to use.
+(defvar litex-math-inline-start "\\(")
+(defvar litex-math-inline-end "\\)")
+
+;; math equation environment to use and strings to join the solutions.
+(defvar litex-math-equation-start "\\begin{equation}\n")
+(defvar litex-math-equation-end "\n\\end{equation}\n")
+(defvar litex-math-steps-equation-join-string "= ")
+(defvar litex-math-steps-equation-end-string " ")
+
+;; math eqnarray environment to use and strings to join the solutions.
+(defvar litex-math-eqnarray-start "\\begin{eqnarray*}\n")
+(defvar litex-math-eqnarray-end "\n\\end{eqnarray*}\n")
+(defvar litex-math-steps-eqnarray-join-string " &=& ")
+(defvar litex-math-steps-eqnarray-end-string "\\\\\n")
+
 
 (defun litex-format-float (val)
-  "Function that defines how floats are formatted in lisp2latex."
-  (if (or (< val 1e-2) (> val 1e4))
-                (let* ((exponent (floor (log val 10)))
-                      (front (/ val (expt 10 exponent))))
-                  (format "%.2f \\times 10^{%d}" front exponent))
-    (format "%.3f" val)))
+  "Function that defines how float VAL is formatted in lisp2latex."
+  (if (or (< val litex-format-float-lower-limit) (> val litex-format-float-upper-limit))
+      (let* ((exponent (floor (log val 10)))
+             (front (/ val (expt 10 exponent))))
+        (format (concat litex-format-float-string " \\times 10^{%d}") front exponent))
+    (format litex-format-float-string val)))
 
 
 (defun litex-latex-maybe-enclose (form)
+  "Encloses FORM in parantheis if LITEX-LATEX-MAYBE-ENCLOSE is true."
   (let* ((litex-latex-maybe-enclose? nil)
          (latex (litex-lisp2latex-all form)))
     (if litex-latex-maybe-enclose?
@@ -59,7 +84,7 @@
 
 
 (defun litex-lisp2latex-all (form)
-  "Converts given lisp expression to latex equivalent"
+  "Convert given Lisp expression FORM to latex equivalent string."
   (pcase form
     ;; basic operators
     (`(+ . ,args)
@@ -70,9 +95,9 @@
      (setf latex-maybe-enclose t)
      (with-output-to-string
        (cl-loop for (me next . rest) on args do
-             (if (numberp next)
-                 (princ (format "%s \\times " (litex-latex-maybe-enclose me)))
-               (princ (format "%s " (litex-latex-maybe-enclose me)))))))
+		(if (numberp next)
+                    (princ (format "%s \\times " (litex-latex-maybe-enclose me)))
+		  (princ (format "%s " (litex-latex-maybe-enclose me)))))))
 
     (`(/ ,a1 . ,args)
      (if args
@@ -95,8 +120,8 @@
     (`(setq . ,args)
      (with-output-to-string
        (cl-loop for (a b . rest) on args by #'cddr do
-             (princ (format "%s = %s" (litex-lisp2latex-all a) (litex-lisp2latex-all b)))
-             (when rest (princ "; ")))))
+		(princ (format "%s = %s" (litex-lisp2latex-all a) (litex-lisp2latex-all b)))
+		(when rest (princ "; ")))))
 
     ;; other operators
     (`(1+ ,arg) (concat "1 + " (litex-lisp2latex-all arg) ))
@@ -106,7 +131,7 @@
     ;; Function definition:
     ;; TODO: something like func(a,b,c): a+b+c
 
-    
+
     ;; named functions
     (`(,func . ,args)
      (let* ((known? (cl-find func litex-latex-functions))
@@ -125,18 +150,18 @@
 
 
 (defun litex-contains-variables (expression)
-  "Checks if given expression has variables."
+  "Check if given EXPRESSION has variables."
   (if (functionp expression)
       nil
-  (if (symbolp expression)
+    (if (symbolp expression)
 	t
       (if (consp expression)
 	  (cl-some #'litex-contains-variables expression)
-      nil))))
+	nil))))
 
 
 (defun litex-substitute-values (expression)
-  "gives a string from expression substituting the values."
+  "Gives a string from EXPRESSION substituting the values."
   (if (functionp expression)
       (format "%s" expression)
     (if (symbolp expression)
@@ -148,6 +173,7 @@
 
 
 (defun litex-solve-single-step (form)
+  "Solves a single step of calculation in FORM."
   (cond ((listp form)
          (if (cl-every #'numberp (cl-rest form))
              (eval form)
@@ -162,34 +188,36 @@
 
 
 (defun litex-solve-all-steps (form)
+  "Solves all the steps of calculations in FORM expression and retuns a list of steps."
   (let
       ((solution (list form))) ;given expression
     (if
 	(litex-contains-variables form)
+	(setq solution
+	      (append solution
+		      (list
+		       (setq form
+			     (read
+			      (litex-substitute-values form)))))))
+
+    (while (consp form)
       (setq solution
 	    (append solution
-		    (list
-		     (setq form
-			   (read
-			    (litex-substitute-values form)))))))
-
-  (while (consp form)
-    (setq solution
-	  (append solution
-		  (list (setq form
-			      (litex-solve-single-step form))))))
-  solution))
+		    (list (setq form
+				(litex-solve-single-step form))))))
+    solution))
 
 
 (defun litex-sexp-to-solved-string (expression format-func)
+  "Return solved string of EXPRESSION using FORMAT-FUNC to format all steps."
   (pcase expression
-      (`(setq ,var ,exp)
-       (concat
-	(format (concat "%s" litex-steps-join-string) var)
-	(mapconcat format-func (litex-solve-all-steps exp)
-		   (concat litex-steps-end-string litex-steps-join-string))))
-      (_ (mapconcat format-func (litex-solve-all-steps expression)
-		    (concat litex-steps-end-string litex-steps-join-string)))))
+    (`(setq ,var ,exp)
+     (concat
+      (format (concat "%s" litex-steps-join-string) var)
+      (mapconcat format-func (litex-solve-all-steps exp)
+		 (concat litex-steps-end-string litex-steps-join-string))))
+    (_ (mapconcat format-func (litex-solve-all-steps expression)
+		  (concat litex-steps-end-string litex-steps-join-string)))))
 
 
 (defun litex-eval-and-replace ()
@@ -204,107 +232,118 @@
 
 
 (defun litex-eval-and-insert ()
-  "Inserts the evaulation result of the preceding sexp."
+  "Insert the evaulation result of the preceding sexp."
   (interactive)
   (backward-kill-sexp)
   (condition-case nil
       (insert (concat (current-kill 0)
-		     litex-steps-join-string
-		     (format "%s"
-			     (eval (read (current-kill 0))))))
+		      litex-steps-join-string
+		      (format "%s"
+			      (eval (read (current-kill 0))))))
     (error (message "Invalid expression"))))
 
 
 (defun litex-exp-to-latex (beg end)
-  "Converts exponentials to latex notation."
+  "Convert exponentials in the given region to latex notation.
+Argument BEG begining position of region.
+Argument END end position of region."
   (interactive "r")
   (save-excursion
     (goto-char end)
     (while (re-search-backward
-     "\\([0-9.+-]+\\)e\\([0-9.+-]+\\)" beg)
+	    "\\([0-9.+-]+\\)e\\([0-9.+-]+\\)" beg)
       (replace-match "\\1 \\\\times 10^{\\2}"))))
 
 
 (defun litex-exp-in-latex-math (beg end)
-  "Inserts the selected expression inside latex inline math environment."
+  "Insert the selected expression inside latex inline math environment.
+Argument BEG begining position of region.
+Argument END end position of region."
   (interactive "r")
   (save-excursion
     (goto-char beg)
-    (insert "\\(")
+    (insert litex-math-inline-start)
     (goto-char (+ (point) (- end beg)))
-    (insert "\\)")))
+    (insert litex-math-inline-end)))
 
 
 (defun litex-sexp-to-latex-exp ()
-  "Converts valid sexp to latex expressions."
+  "Convert valid sexp to latex expressions."
   (interactive)
   (backward-kill-sexp)
   (insert (litex-lisp2latex-all (read (current-kill 0)))))
 
 
 (defun litex-sexp-replace-variables ()
+  "Replace the variable values in the last sexp at point."
   (interactive)
   (backward-kill-sexp)
   (insert (litex-substitute-values (read (current-kill 0)))))
 
 
-(defun litex-sexp-solve-all-steps (expression)
-  (interactive (list (sexp-at-point)))
+(defun litex-sexp-solve-all-steps ()
+  "Solve last sexp at point in steps and insert those steps."
+  (interactive)
   (backward-kill-sexp)
-  (let ((expression (read (current-kill 0))))  
+  (let ((expression (read (current-kill 0))))
     (insert
      (litex-sexp-to-solved-string expression #'prin1-to-string))))
 
 
 (defun litex-sexp-solve-all-steps-equation ()
+  "Solve last sexp in steps and insert it in LaTeX equation environment."
   (interactive)
   (backward-kill-sexp)
   (let ((expression (read (current-kill 0)))
-	(litex-steps-join-string "= ")
-	(litex-steps-end-string " "))
-    (insert "\\begin{equation}\n")
+	(litex-steps-join-string litex-math-steps-equation-join-string)
+	(litex-steps-end-string litex-math-steps-equation-end-string))
+    (insert litex-math-equation-start)
     (insert
      (litex-sexp-to-solved-string expression #'litex-lisp2latex-all))
-    (insert "\n\\end{equation}\n")))
+    (insert litex-math-equation-end)))
 
 
 (defun litex-sexp-solve-all-steps-eqnarray ()
+  "Solve last sexp in steps and insert it in LaTeX eqnarray environment."
   (interactive)
   (backward-kill-sexp)
   (let ((expression (read (current-kill 0)))
-	(litex-steps-join-string " &=& ")
-	(litex-steps-end-string "\\\\\n"))
-    (insert "\\begin{eqnarray*}\n")
+	(litex-steps-join-string litex-math-steps-eqnarray-join-string)
+	(litex-steps-end-string litex-math-steps-eqnarray-end-string))
+    (insert litex-math-eqnarray-start)
     (insert
      (litex-sexp-to-solved-string expression #'litex-lisp2latex-all))
-    (insert "\n\\end{eqnarray*}\n")))
+    (insert litex-math-eqnarray-end)))
 
 
 (defun litex-format-region-last (beg end)
-      (interactive (if (use-region-p)
+  "Format selected region as per format of last call to `litex-format-region`,BEG and END are region bounds."
+  (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
                  (let ((bnd (bounds-of-thing-at-point 'sexp)))
 		   (list (cl-first bnd) (cl-rest bnd)))))
-      (let ((text (buffer-substring-no-properties beg end)))
-	;; maybe I should make it eval if given expression
-      (if (string-match-p "%[0-9.]*[dfex]" litex-format-string)
-	    (setq text (eval (read text))))
-      (kill-region beg end)
-      (insert (format litex-format-string text))))
+  (let ((text (buffer-substring-no-properties beg end)))
+    ;; maybe I should make it eval if given expression
+    (if (string-match-p "%[0-9.]*[dfex]" litex-format-float-string-float)
+	(setq text (eval (read text))))
+    (kill-region beg end)
+    (insert (format litex-format-float-string-float text))))
 
 
 (defun litex-format-region (beg end)
-    (interactive (if (use-region-p)
+  "Format selected region as per input format, BEG and END are region bounds."
+  (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
                  (let ((bnd (bounds-of-thing-at-point 'symbol)))
 		   (list (cl-first bnd) (cl-rest bnd)))))
-    (let ((fmt  (read-string "Enter format string:"
-			     litex-format-string)))
-      (setq litex-format-string fmt)
-      (litex-format-region-last beg end)))
+  (let ((fmt  (read-string "Enter format string:"
+			   litex-format-float-string-float)))
+    (setq litex-format-float-string-float fmt)
+    (litex-format-region-last beg end)))
 
 
 (defun litex-increment-number (step)
+  "Increase the number value by STEP."
   (interactive "P")
   (let* ((bounds (if (use-region-p)
                      (cons (region-beginning) (region-end))
@@ -328,17 +367,15 @@
 
 
 (defun litex-insert-or-replace-x (beg end)
-  "If a region is selected, replaces * by \times otherwise inserts \times
-instead of ×. Useful in LaTeX or to convert mathmatical expression
-to human redable one."
+  "If a region (BEG to END) is selected, replace * by \times otherwise insert \times instead of ×."
   (interactive (if (use-region-p)
                    (list (region-beginning) (region-end))
                  (list (point) (point))))
   (goto-char end)
   (if (= beg end)
       (insert "\\times")
-      (when (re-search-backward "*" beg)
-	(replace-match "\\\\times"))))
+    (when (re-search-backward "*" beg)
+      (replace-match "\\\\times"))))
 
 
 (setq litex-key-map (make-sparse-keymap))
