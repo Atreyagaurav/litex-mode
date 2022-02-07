aaaa1144a2d14081c905096306ad50860db64334
Gaurav <allmanpride@gmail.com>
2022-02-07 13:36:16 -0500

    Add texinfo

--- litex-mode.el
+++ litex-mode.el
@@ -28,6 +28,8 @@
 ;; improved from https://emacs.stackexchange.com/a/70360
 ;; modified with help from https://gist.github.com/bpanthi977/4b8ece0eeff3bc05bb82275a23cbb56d
 
+;; For detailed help visit github page:  https://github.com/Atreyagaurav/litex-mode
+
 ;;; Code:
 (eval-when-compile (require 'pcase))
 (require 'cl-lib)
