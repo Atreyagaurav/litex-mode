973598d2dcbcab493e1f99dfbf8c21de3113ac58
Gaurav <allmanpride@gmail.com>
2022-09-03 17:06:59 -0400

    Revert "Add auxiliary functions for units"
    This reverts commit 73213a012c9f2c23fe443c54f40d9acf3d2f0ad7.

--- tests/units-tests.el
+++
@@ -1,9 +0,0 @@
-;; Run all tests using (ert t)
-
-;; conversion tests
-(ert-deftest units-convert-test ()
-  (should (= (units-convert 1 "ft" "m") 0.3048))
-  (should (= (units-convert 1 "m" "ft") 3.2808399))
-  (should (= (units-convert 1 "kg" "g") 1e3))
-  (should (= (units-convert 1 "hour" "seconds") (* 60 60)))
-  (should (= (units-convert 1 "day" "hour") 24)))
